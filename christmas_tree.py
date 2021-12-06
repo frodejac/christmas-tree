59e41ec719372c58f049a42269a69da7bf2bccc7
Frode Jacobsen <fj@itverket.no>
2021-12-06 12:02:52 +0100

    Initial commit

---
+++ christmas_tree.py
@@ -0,0 +1,25 @@
+_=                                      (
+                                       o:=
+                                   __builtins__
+                               .__dict__["range"]
+                             )and list((p:=__import__)
+                           and(g:=getattr)and(sl:=g(p(t_
+                         :="time"),"sleep")) and map(lambda
+                      s:sl(1/15)or print(f"\033[s{s}\n\033[1;3"
+                    f"7m{'*'*0x50}\033[0m\033[0K\033[u",end=""),(
+                  m:=map)and(l_:=__builtins__.__dict__.get("".join(
+                list(map(lambda _:chr(_),[108,105,115,116])))))and m(
+              lambda _:g("",j_:="join")(l_((q:=p(r_:="random"))and(l:=
+            g(q,"seed"))and(r:=g(q,r_))and m(lambda s:g(g((_:=chr(27))and
+          {**{k:f"{_}[0;33m{{}}{_}[0m" for k in "\\/-<+>|"},**{k:f"{_}[0;31"
+         f"m{{}}{_}[0m" for k in "@&$#"},"*":f"{_}[1;37m{{}}{_}[0m","^":f"{_}"
+       f"[0;32m{{}}{_}[0m",},"get",)(s,"{}"),g("",j_)(l_(m(lambda x: chr(x),[102
+      ,111,114,109,97,116]))),)(s),m(lambda a,b:a if b or a=="\n" else "*",g("\n",
+    j_)(l_(m(lambda s:f"{s:^{20*4}}",["","","\\|/","--<+>--","/|\\"],))+l_( m(lambda
+   e:(f:=p("functools"))and g(f,"reduce")(lambda s,k:(c:= g(p(r_),"choice"))and(s[:k]+
+ c("@&$#")+s[k+1:]),*g(__builtins__,"reversed")(e),),l(1)or[(f"{'^'*(i*2-1):^{20*4}}",[(
+20*2)-1+i-j*2for j in o(i)if r()>0.7],)for i in o(1,20)],))),l((t:=g(p(t_),t_))and t())or
+                                  (r()<0x13/0x14
+                                  for _ in iter(
+                                  int,1)))))),(_
+                               for _ in o(0x78)))))
